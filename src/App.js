f877bc7248f0eba819ddd46b550855a610d53f81
solomonsanthosh <solosanty2064@gmail.com>
2023-06-16 18:45:36 +0530

    SS

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
-
+import Calender from './pages/home/Calender';
+import Roadmap from './pages/home/Roadmap'
+import Roadmap2 from './pages/home/Roadmap2'
+import Roadmap3 from './pages/home/Roadmap3'
 function App() {
   return (
     <>
@@ -11,6 +14,8 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
+          <Route path="/calender" element={<Calender />}></Route>
+          <Route path="/roadmap" element={<Roadmap3 />}></Route>
         </Routes>
       </Router>
     </>
