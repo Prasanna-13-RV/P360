06e003ef88e33d3d67b18e0dcd09593132b7a160
Prasanna <prasannavelmurugan0200@gmail.com>
2023-06-17 17:10:13 +0530

    rv

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/Home';
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
-import ViewInternship from './pages/internship/ViewInternship';
-import AddInternship from './pages/internship/AddInternship';
-import Filter from './pages/filter/Filter';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import ViewInternship from "./pages/internship/ViewInternship";
+import Filter from "./pages/filter/Filter";
+import Profile from "./pages/profile/Profile";
+import ProfileEdit from "./pages/profile/ProfileEdit";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          {/* auth */}
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          {/* internship */}
-          <Route path="/internship" element={<ViewInternship />}></Route>
-          <Route path="/internship/create" element={<AddInternship />}></Route>
-          {/* Filter */}
-          <Route path="/filter" element={<Filter />}></Route>
-        </Routes>
-      </Router>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    {/* auth */}
+                    <Route path="/login" element={<Login />}></Route>
+                    <Route path="/register" element={<Register />}></Route>
+                    {/* internship */}
+                    <Route
+                        path="/internship"
+                        element={<ViewInternship />}
+                    ></Route>
+                    {/* Filter */}
+                    <Route path="/filter" element={<Filter />}></Route>
+                    {/* Profile */}
+                    <Route path="/profile" element={<Profile />}></Route>
+                    <Route
+                        path="/profile/update"
+                        element={<ProfileEdit />}
+                    ></Route>
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
