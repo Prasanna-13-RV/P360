2115d18dd358078bd931ed6c63195065e3d3d578
lohithsrk <srklohith05@gmail.com>
2023-06-23 10:23:15 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -20,47 +20,47 @@ import Calender from "./pages/admin/Calender/Calender";
 import Roadmap from "./pages/roadmap/Roadmap";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          {/* auth */}
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          {/* internship */}
-          <Route path="/internship" element={<ViewInternship />}></Route>
-          {/* Filter */}
-          {/* <Route path="/filter" element={<Filter />}></Route> */}
-          {/* Profile */}
-          <Route path="/profile" element={<Profile />}></Route>
-          <Route path="/profile/update" element={<ProfileEdit />}></Route>
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    {/* auth */}
+                    <Route path="/login" element={<Login />}></Route>
+                    <Route path="/register" element={<Register />}></Route>
+                    {/* internship */}
+                    <Route path="/internship" element={<ViewInternship />}></Route>
+                    {/* Filter */}
+                    {/* <Route path="/filter" element={<Filter />}></Route> */}
+                    {/* Profile */}
+                    <Route path="/profile" element={<Profile />}></Route>
+                    <Route path="/profile/update" element={<ProfileEdit />}></Route>
 
-          {/* Filter */}
-          {/* <Route path="/student" element={<Filter />}></Route> */}
-          {/* Profile */}
+                    {/* Filter */}
+                    {/* <Route path="/student" element={<Filter />}></Route> */}
+                    {/* Profile */}
 
-          {/* admin */}
-          <Route path="/admin/login" element={<LoginAdmin />}></Route>
-          {/* internship */}
-          <Route path="/admin/internship" element={<Internship />}></Route>
-          {/* file upload */}
-          <Route path="/file" element={<FileUpload />}></Route>
-          <Route path="/admin/student/file" element={<StudentFile />}></Route>
-          <Route path="/admin/marks/file" element={<MarksFile />}></Route>
-          {/* Roadmap */}
-          <Route path="/admin/addroadmap" element={<AddRoadmap />}></Route>
-          <Route
-            path="/admin/updateroadmap"
-            element={<UpdateRoadmap />}></Route>
-          <Route path="/admin/showroadmap" element={<ShowRoadmaps />}></Route>
-          <Route path="/admin/roadmap" element={<Roadmap />}></Route>
+                    {/* admin */}
+                    <Route path="/admin/login" element={<LoginAdmin />}></Route>
+                    {/* internship */}
+                    <Route path="/admin/internship" element={<Internship />}></Route>
+                    {/* file upload */}
+                    <Route path="/file" element={<FileUpload />}></Route>
+                    <Route path="/admin/student/file" element={<StudentFile />}></Route>
+                    <Route path="/admin/marks/file" element={<MarksFile />}></Route>
+                    {/* Roadmap */}
+                    <Route path="/admin/addroadmap" element={<AddRoadmap />}></Route>
+                    <Route
+                        path="/admin/updateroadmap"
+                        element={<UpdateRoadmap />}></Route>
+                    <Route path="/admin/showroadmap" element={<ShowRoadmaps />}></Route>
+                    <Route path="/admin/roadmap" element={<Roadmap />}></Route>
 
-          <Route path="/admin/dashboard" element={<StudentDashboard />}></Route>
-          <Route path="/admin/calender" element={<Calender />}></Route>
-        </Routes>
-      </Router>
-    </>
-  );
+                    <Route path="/admin/dashboard" element={<StudentDashboard />}></Route>
+                    <Route path="/admin/calender" element={<Calender />}></Route>
+                </Routes>
+            </Router>
+        </>
+    );
 }
 export default App;
