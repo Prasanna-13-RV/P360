0bc4269f06b25dffecaaeed834151ff096698dcd
Prasanna <prasannavelmurugan0200@gmail.com>
2023-08-24 10:23:03 +0530

    rv

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
 
           {/* Profile */}
           <Route path="/profile/:id" element={<Profile user={user} />}></Route>{/* Verified*/}
-          <Route path="/profile/update/:id" element={<ProfileEdit />}></Route> {/* Verified*/}
+          <Route path="/profile/update/:id" element={<ProfileEdit user={user}/>}></Route> {/* Verified*/}
 
           <Route path="/profile/resume/:id" element={<ProfilePage />}></Route>
           {/* internship */}
