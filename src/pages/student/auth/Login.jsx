55ebc66b27108ac076ed3001a2f3d0b6ddaed8bc
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2023-08-28 11:41:46 +0530

    Changes

--- src/pages/student/auth/Login.jsx
+++ src/pages/student/auth/Login.jsx
@@ -39,7 +39,7 @@ function Login() {
 
     return (
         <>
-            <Navbar />
+            {/* <Navbar /> */}
             <section className="bg-gray-50">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <a
